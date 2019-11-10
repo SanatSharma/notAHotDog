19d4de8caa1a7a3ab06c2c8702bc3b04784d2f4a
Sanat Sharma <sanatcolumban@gmail.com>
2019-11-10 17:31:31 -0600

    Change dataset skeleton

--- data/dataset.py
+++ data/dataset.py
@@ -1,6 +1,9 @@
 import torch
 from torchvision import transforms, utils
 from torch.utils.data import DataSet, DataLoader
+from utils import *
+import numpy as np
+from PIL import image
 
 class NSFWDataset(DataSet):
     def __init__(self, data_path_nsfw, data_path_neutral):
@@ -10,7 +13,40 @@ class NSFWDataset(DataSet):
                             transforms.Normalize(mean=[0.485, 0.456, 0.406], 
                                     std=[0.229, 0.224, 0.225])])
         self.nsfw_dirs = data_path_nsfw
-        self.neutral_dir = data_path_neutral        
+        self.neutral_dir = data_path_neutral   
+        self.indexer = Indexer()        
+        self.labels = create_dataset()
 
     def __len__(self):
-        return 
+        return len(self.indexer)
+    
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        img = image.open(self.indexer.get_object(idx))
+        img = self.transform(img)
+        label = self.labels[idx]
+        
+        return (image, label)
+
+    def create_dataset(self):
+        labels = []
+        # TODO: Slow to append every time. Should I just loop through and get the length in one go?
+        for nsfw_dir in self.nsfw_dirs:
+            files = [os.path.join(nsfw_dir, p) for p in sorted(os.listdir(nsfw_dir))]
+            for file in files:
+                self.indexer.get_index(file)
+                labels.append(1)
+        
+        files = [os.path.join(self.neutral_dir, p) for p in sorted(os.listdir(self.neutral_dir))]
+        for file in files:
+            self.indexer.get_index(file)
+            labels.append(0)
+
+        return np.array(labels)
+
+        
+
+
+        
