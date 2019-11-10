26a8b87e81b9b6f1100bda50441d485bc98861cb
Sanat Sharma <sanatcolumban@gmail.com>
2019-11-10 16:07:52 -0600

    Add additional command line options

--- nsfw.py
+++ nsfw.py
@@ -3,8 +3,12 @@ from models.inception import *
 
 def arg_parse():
     parser = argparse.ArgumentParser(description='trainer.py')
-    parser.add_argument('--data_path', type=str, default='data', help='Path to the training set')
+    parser.add_argument('--data_path', type=str, default='data/amateur/IMAGES', help='Path to the training set')
     parser.add_argument('--model', type=str, default='inceptionv3', help='Model to run')
+    parser.add_argument('--epochs', type=int, default=5)
+    parser.add_argument('--lr', type=float, default=0.001, help="learning rate for optimizer")
+    parser.add_argument('--batch_size', type=int, default=32, help='Batch size for training')
+    parser.add_argument('--test_batch_size', type=int, default=8, help='Batch size for testing')
 
     args = parser.parse_args()
     return args
