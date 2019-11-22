e0879fbf7cc53c6b869598c88fed451bd7aa4231
Sanat Sharma <sanatcolumban@gmail.com>
2019-11-22 14:35:19 -0800

    fix server bugs

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import sys
 import flask
 from flask_cors import CORS
 from torchvision import transforms, utils
+from torch.utils.data.sampler import SequentialSampler
 from PIL import Image
 import torch
 import numpy as np
@@ -38,9 +39,12 @@ if __name__ == '__main__':
     # This is used when running locally. Gunicorn is used to run the
     # application on Google App Engine. See entrypoint in app.yaml.
     
-    model_path = "data/trained.pt"
+    model_path = "models/trained.pt"
     model = InceptionV3()
-    model.load_state_dict(torch.load(args.model_path))
+    if torch.cuda.is_available():
+        model.load_state_dict(torch.load(model_path))
+    else:
+        model.load_state_dict(torch.load(model_path, map_location='cpu'))
     trained_model = Trained_Model(model)
     app.run(host='127.0.0.1', port=8080, debug=True)
 
